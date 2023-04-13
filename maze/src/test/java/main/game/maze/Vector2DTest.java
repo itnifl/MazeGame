c6970401279f069d6bd0ddf6ecf7310721575498
Atle Holm <atle@team-holm.net>
2023-04-14 01:08:48 +0200

    Update Vector2DTest.java

--- maze/src/test/java/main/game/maze/Vector2DTest.java
+++ maze/src/test/java/main/game/maze/Vector2DTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.DisplayName;
 public class Vector2DTest {
     @Test
     public void TwoVectorsIntersect_true() {
-        Vector2D vector1 = new Vector2D(0, 150, 0, 160);
-        Vector2D vector2 = new Vector2D(0, 150, 0, 160);
+        Vector2D vector1 = new Vector2D(0, 0, 100, 100);
+        Vector2D vector2 = new Vector2D(100, 0, 0, 100);
         
         var intersect = vector1.doIntersect(vector2, 1);
 
