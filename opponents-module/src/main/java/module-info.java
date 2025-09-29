077af9919363bb06dbc5e640c7fd9122c6918321
Atle Holm <atle@itautomasjon.no>
2025-09-29 19:00:49 +0200

    Making sure Ecore validators are available to other modules

--- opponents-module/src/main/java/module-info.java
+++ opponents-module/src/main/java/module-info.java
@@ -3,5 +3,6 @@ module main.game.maze.opponents {
     requires org.eclipse.emf.ecore;
     // keep exports/opens as before
     exports main.game.maze.opponents;
+    exports main.game.maze.opponents.util;
     opens main.game.maze.opponents to org.eclipse.emf.ecore;
 }
