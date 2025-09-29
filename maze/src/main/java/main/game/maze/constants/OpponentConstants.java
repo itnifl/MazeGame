533039afd01f7d05ebab72c900f5c31bc93c4b1a
Atle Holm <atle@itautomasjon.no>
2025-09-29 19:55:50 +0200

    Removing unused constants

--- maze/src/main/java/main/game/maze/constants/OpponentConstants.java
+++ maze/src/main/java/main/game/maze/constants/OpponentConstants.java
@@ -1,9 +1,5 @@
 package main.game.maze.constants;
 
 public class OpponentConstants {
-    public final static String ZombieModelPath = "/opponents/instances/classic_zombie.xmi";
-    public final static String ZombieBaseGraphic = "/main/game/maze/zombie.png";
-    public final static String ZombieBaseGraphicLeftTurn = "/main/game/maze/zombie-left.png";
-    public final static String ZombieBaseGraphicRightTurn = "/main/game/maze/zombie-right.png";
-    
+    public final static String ZombieModelPath = "/opponents/instances/classic_zombie.xmi";    
 }
