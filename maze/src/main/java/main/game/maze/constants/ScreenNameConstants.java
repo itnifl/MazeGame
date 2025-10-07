85b1bbd549b4d34097cda57d546d0cc7182117a7
Atle Holm <atle@itautomasjon.no>
2025-10-08 00:10:53 +0200

    Update ScreenNameConstants.java

--- maze/src/main/java/main/game/maze/constants/ScreenNameConstants.java
+++ maze/src/main/java/main/game/maze/constants/ScreenNameConstants.java
@@ -2,8 +2,7 @@ package main.game.maze.constants;
 
 public class ScreenNameConstants {
     public final static String GameOverScreen = "/main/game/maze/gameOverScreen.fxml";
-    public final static String WinGameScreen = "/main/game/maze/winGameScreen.fxml";
+    public final static String WinGameScreen = "/main/game/maze/winScreen.fxml";
     public final static String HighscoreScreen = "/main/game/maze/highscoreScreen.fxml";
     public final static String GameScreen = "/main/game/maze/game.fxml";
-
 }
