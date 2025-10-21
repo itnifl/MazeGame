59fc4d8dd5618822a84ddaf1f10b4a8f2d6c6aee
Nathan Vernois <nlvernoi@stud.ntnu.no>
2025-10-21 18:59:36 +0200

    Added module for movements

---
+++ movements-module/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module main.game.maze.opponents {
+    requires org.eclipse.emf.common;
+    requires org.eclipse.emf.ecore;
+    // keep exports/opens as before
+    opens main.game.maze.opponents to org.eclipse.emf.ecore;
+}
