aa9ae59091f9c4f62bd94437e20abbe06fe3e57c
Atle Holm <atleholm@stud.ntnu.no>
2025-11-12 12:56:36 +0100

    Removing temporary workfiles

--- local_work/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module main.game.maze.behaviours {
-    requires org.eclipse.emf.common;
-    requires org.eclipse.emf.ecore;
-    requires main.game.maze.opponents;
-
- 
-    exports main.game.maze.behaviours;
-    opens main.game.maze.behaviours to org.eclipse.emf.ecore;
-}
