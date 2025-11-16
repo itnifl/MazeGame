931c3e1384511f961f4d23b26c6223a24e096b66
Atle Holm <atleholm@stud.ntnu.no>
2025-11-16 12:22:12 +0100

    Adjusting for Acceleo

--- maze-generator.acceleo/src/main/java/main/game/maze/gen/HeadlessGeneratorApp.java
+++ maze-generator.acceleo/src/main/java/main/game/maze/gen/HeadlessGeneratorApp.java
@@ -12,13 +12,16 @@ public class HeadlessGeneratorApp implements IApplication {
   @Override
   public Object start(IApplicationContext context) throws Exception {
     String[] args = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+    
     if (args == null || args.length < 2) {
       System.err.println("Usage: -application main.game.maze.gen.app <input.xmi> <outDir>");
       return IApplication.EXIT_OK;
     }
+
     String modelPath = new File(args[0]).getAbsolutePath();
     String outDir    = new File(args[1]).getAbsolutePath();
     new RunAcceleo().run(modelPath, outDir);
+    
     return IApplication.EXIT_OK;
   }
   @Override public void stop() {}
